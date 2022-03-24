7bd08ba9c1680c0942ab82ee7041f3ae6c61e859
Philip <fhp.3858@gmail.com>
2022-03-24 17:20:05 -0600

    primer commit

---
+++ src/test/java/com/entity/cliente/azteca/EntityServiceClienteV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entity.cliente.azteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityServiceClienteV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
